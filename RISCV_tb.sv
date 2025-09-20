d98b2cb9b9db65e87c67eb166e5913fe91bb5480
Aaryan Dhawan <aaryanvaldhawan@gmail.com>
2025-09-20 12:56:25 -0400

    Updated Testbench

--- RISCV_tb.sv
+++ RISCV_tb.sv
@@ -49,65 +49,32 @@ module RISV_tb;
         reset = 0;
         start = 1;
 
-        repeat (36) begin
+        repeat (32) begin
+            @(posedge clk);
+        end
+        start = 0;
+        Imem_write_instr = 32'h00AE0E13; 
+        Up = 1;
+
+        repeat (3) begin
+            @(posedge clk);
+        end
+        Imem_write_en = 1;
+        
+        @(posedge clk);
+        Imem_write_en = 0;
+        Up = 0;
+        Down = 1;
+
+        repeat (4) begin
+            @(posedge clk);
+        end
+        Down = 0;
+        start = 1;
+        repeat (10) begin
             @(posedge clk);
         end
         $finish;
         $stop;
     end
-    // Test stimulus
-    // initial begin
-    //     $timeformat(-9, 2, " ns", 20);
-    //     start = 0;
-    //     Imem_write_instr = 32'b0;
-    //     Imem_write_en = 0;
-    //     Up = 0;
-    //     Down = 0;
-    //     reset = 0;
-
-    //     @(posedge clk);
-    //     reset = 1;  
-    
-    //     @(posedge clk);
-    //     reset = 0;
-    //     start = 1;
-    //     // assert(dut.write_back_data == 32'd5) else $error("Write back data is not 5 at initial instruction");
-    //     // $display("Write back data after first instruction: %0d", dut.write_back_data);
-    //     // $display("Current simulation time (integer): %t", $time);
-        
-    //     // @(posedge clk);
-    //     // $display("Write back data after first instruction: %0d", dut.write_back_data);
-    //     // $display("Current simulation time (integer): %t", $time);
-    //     // assert(dut.write_back_data == 32'd8) else $error("Write back data is not 8 at second instruction");
-    //     // start = 0;
-
-    //     // // Write a new instruction to address 4
-    //     // Imem_write_instr = 32'h00AE0E13; 
-    //     // Up = 1;
-
-    //     @(posedge clk);
-    //     @(posedge clk);
-    //     @(posedge clk);
-    //     Imem_write_en = 1;
-        
-    //     @(posedge clk);
-    //     Imem_write_en = 0;
-    //     Up = 0;
-    //     Down = 1;
-
-    //     repeat (4) begin
-    //         @(posedge clk);
-    //     end
-    //     Down = 0;
-    //     start = 1;
-    //     // assert(dut.write_back_data == 13) else $error("Write back data is not 13 at third instruction");
-    //     @(posedge clk);
-    //     // assert(dut.write_back_data == 10) else $error("Write back data is not 10 at fourth instruction");
-    //     repeat (2) begin
-    //         @(posedge clk);
-    //     end
-    //     $finish;
-    //     $stop;
-    // end
-
 endmodule
